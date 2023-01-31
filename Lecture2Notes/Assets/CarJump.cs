613996b411d829e81de1855fe326a62891038e30
jamie mason <90934296+jamie-mason@users.noreply.github.com>
2023-01-31 06:09:15 -0500

    Add files via upload

---
+++ Lecture2Notes/Assets/CarJump.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarJump : MonoBehaviour
+{
+    private CharacterController characterController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
